d42a8f4c6741892631678ba0e5a911fe259754af
iamzidan <zidann256@gmail.com>
2025-12-15 08:10:36 +0200

    movie model added

---
+++ lib/models/movie.dart
@@ -0,0 +1,42 @@
+import 'package:cenima/models/app_config.dart';
+import 'package:get_it/get_it.dart';
+
+class Movie {
+  final String name;
+  final String language;
+  final bool isAdult;
+  final String description;
+  final String posterPath;
+  final String backdropPath;
+  final num rating;
+  final String releaseDate;
+
+  Movie({
+    required this.name,
+    required this.language,
+    required this.isAdult,
+    required this.description,
+    required this.posterPath,
+    required this.backdropPath,
+    required this.rating,
+    required this.releaseDate,
+  });
+
+  factory Movie.fromJson(Map<String, dynamic> jsonData) {
+    return Movie(
+      name: jsonData["title"],
+      language: jsonData["original_language"],
+      isAdult: jsonData["adult"],
+      description: jsonData["overview"],
+      posterPath: jsonData["poster_path"],
+      backdropPath: jsonData["backdrop_path"],
+      rating: jsonData["vote_average"],
+      releaseDate: jsonData["release_date"],
+    );
+  }
+
+  String posterURl() {
+    final AppConfig appConfig = GetIt.instance.get<AppConfig>();
+    return "${appConfig.baseImageApiUrl}$posterPath";
+  }
+}
